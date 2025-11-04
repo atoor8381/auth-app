d4b534441cf052829e48930fc0e12e0d66f97d71
atoor0807 <atoor0807@gmail.com>
2025-11-04 07:18:50 +0500

    login setup

--- backend/Controllers/authcontrollers.js
+++ backend/Controllers/authcontrollers.js
@@ -1,4 +1,5 @@
 import { usermodel } from "../Models/user.model"
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
 export let signup = async (req, res) => {
@@ -27,5 +28,46 @@ export let signup = async (req, res) => {
             message : "internal server error",
             success : false
         })
+    }
+}
+
+
+export let login = async (req,res)=>{
+    try{
+    let {email, password} = req.body
+    let user = await usermodel.findOne({email})
+    if(!user){
+        res.status(403).json({
+            message : "User not found. Sign up to become a user",
+            success : false
+        })
+    }
+    let passwormatch = await bcrypt.compare(password, user.password)
+    if(!passwormatch){
+        res.status(403).json({
+            message : "Password invalid",
+            success : false 
+        })
+    }
+
+    let jwttoken = jwt.sign(
+        {user_email : user.email, user_id : user.id},
+        process.env.jwt_secret,
+        {expiresIn : '24h'}
+    )
+
+    res.status(200).json({
+        message : "User logged in",
+        success : true,
+        jwttoken,
+        email,
+        name : user.name
+    })
+        } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
     }
 }
